187a1b49fe13c84c72d792391d000103083cf856
walker2023-glitch <dallaswalker1023@gmail.com>
2026-04-06 17:15:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
 resource "aws_instance" "wingman_app_server" {
-  ami = "ami-0e2c8ccd4e0370c72"
-  instance_type = "t2.micro"             # Free Tier
+  ami           = "ami-0533af068ccd7af1f" # Amazon Linux 2023 (us-east-1)
+  instance_type = "t2.micro"
 
   tags = {
     Name = "RexburgWingman-Production"
   }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.wingman_app_server.public_ip
 }
