44d122a6e2aebad577a31795dad921c10455fba0
walker2023-glitch <dallaswalker1023@gmail.com>
2026-04-06 17:10:57 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "wingman_app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami = "ami-0e2c8ccd4e0370c72"
   instance_type = "t2.micro"             # Free Tier
 
   tags = {
